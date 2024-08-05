866091236968ff7f2e3dc2e94b2cb88b60c90bcb
YanHuan <13781204115@163.com>
2024-08-05 10:07:07 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,15 @@
  * @Author: YanHuan 13781204115@163.com
  * @Date: 2024-06-26 15:34:15
  * @LastEditors: YanHuan 13781204115@163.com
- * @LastEditTime: 2024-07-06 12:59:23
- * @FilePath: \wen-jie-maind:\编程项目\Login\src\App.vue
+ * @LastEditTime: 2024-08-05 10:04:22
+ * @FilePath: \hell0_vued:\编程项目\Login\src\App.vue
  * @Description: 
  * 
  * Copyright (c) 2024 by ${git_name_email}, All Rights Reserved. 
 -->
 <template>
   <RouterView></RouterView>
+  
 </template>
 
 <script lang="ts" setup>
